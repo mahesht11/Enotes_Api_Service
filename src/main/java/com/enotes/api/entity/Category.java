e4e6669c3d8e9d32b94bc3df1a6f2230ac0ea2ac
mahesht11 <maheshgtjava@gmail.com>
2024-11-01 16:34:06 +0530

    Added basic classes

--- src/main/java/com/enotes/api/entity/Category.java
+++ src/main/java/com/enotes/api/entity/Category.java
@@ -3,11 +3,22 @@ package com.enotes.api.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
 @Entity
 public class Category extends BaseModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryId;
+    private String name;
+    private String description;
+
 }
