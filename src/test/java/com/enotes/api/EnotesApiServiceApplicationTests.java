ece933c06fc3d1947001df97aa1716f592d4d9d2
mahesht11 <maheshgtjava@gmail.com>
2024-11-01 16:01:20 +0530

    initial commit

---
+++ src/test/java/com/enotes/api/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.enotes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+
+}
